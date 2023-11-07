b833356b46e2f5a79ee01ff01da9fbd1f0014f0c
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-11-06 17:01:22 -0700

    working router for all but Resume

---
+++ React-Portfolio/src/pages/Contact.jsx
@@ -0,0 +1,27 @@
+export default function Contact() {
+    return (
+        <div>
+            <div>
+                <h2>GitHub</h2>
+                <a href="https://github.com/aestrella0140">https://github.com/aestrella0140</a>
+                <p>
+                    Click the title to be redirected to my GitHub Profile!
+                </p>
+            </div>
+            <div>
+                <h2>Personal Phone Number</h2>
+                <p>
+                    <li>623-755-3094</li>
+                    You can text or call me at anytime after 3PM at the number above.
+                </p>
+            </div>
+            <div>
+                <h2>Email</h2>
+                <a href="mailto: astarfirstborn@hotmail.com">astarfirstborn@hotmail.com</a>
+                <p>
+                    You can email me at any time at the email above.
+                </p>
+            </div>
+        </div>
+    )
+}
--- React-Portfolio/src/pages/Portfolio.jsx
+++ React-Portfolio/src/pages/Portfolio.jsx
@@ -1,15 +1,44 @@
 import { Link } from 'react-router-dom';
-import image from '../assets/notetaker.png';
+import notetaker from '../assets/notetaker.png';
+import pinit from '../assets/PinIt.png';
+import techblog from '../assets/techblog.png';
+import regex from '../assets/regex.png';
 
 export default function Portfolio() {
     return (
     <div>
+        <div className='flex flexbox'>
         <Link
-        to="https://github.com/aestrella0140"
+        to="https://github.com/aestrella0140/Note-Taker-Challenge-9"
         >
             <h1>click me</h1>
-            <img src={image}></img>
+            <img src={notetaker}></img>
         </Link>
+        </div>
+        <div className='flex flexbox'>
+        <Link
+        to="https://github.com/aestrella0140/SVG-Logo-Maker-Challenge-8"
+        >
+            <h1>click me</h1>
+            <img src={pinit}></img>
+        </Link>
+        </div>
+        <div className='flex flexbox'>
+        <Link
+        to="https://github.com/aestrella0140/MVC-Tech-Blog"
+        >
+            <h1>click me</h1>
+            <img src={techblog}></img>
+        </Link>
+        </div>
+        <div className='flex flexbox'>
+        <Link
+        to="https://gist.github.com/aestrella0140/6f4e474cd1e52cc4102bc3c3f8253ad9"
+        >
+            <h1>click me</h1>
+            <img src={regex}></img>
+        </Link>
+        </div>
     </div>
     )
 }
